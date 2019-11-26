8fcfbf40e51e8aa527ee6df05c581c194ca8daea
Alex Price <alex@alexprice.co>
2019-11-25 18:19:46 -0600

    Reset body style, add new page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,38 +1,53 @@
 import * as React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import styled from 'styled-components'
 
 type LayoutProps = {
   title?: string
 }
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD'
-}
+const StyledLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 
 const Layout: React.FunctionComponent<LayoutProps> = ({ children, title }) => (
-  <div style={layoutStyle}>
+  <StyledLayout>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
+
+    <style jsx global>{`
+      body { 
+        background: #000;
+        font: 11px menlo;
+        color: #fff;
+        margin: 0;
+        padding: 0;
+        min-width: 100vw;
+        min-height: 100vh;
+      }
+    `}</style>
+    
     <header>
       <nav>
         <Link href="/">
           <a>Home</a>
         </Link>{' '}
         |{' '}
-        <Link href="/about">
-          <a>About</a>
+        <Link href="/new">
+          <a>Add new...</a>
         </Link>{' '}
         |{' '}
       </nav>
     </header>
     {children}
-  </div>
+  </StyledLayout>
 )
 
 export default Layout
---
+++ pages/new.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const Index: React.FunctionComponent = () => {
+  return (
+    <Layout>
+      <h1>Add a new thing</h1>
+    </Layout>
+  )
+}
+
+export default Index
