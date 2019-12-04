40bdd2ce7dabdc48c12d2724ccbdea07c2692ae9
Alex Price <alex@alexprice.co>
2019-12-04 10:31:23 -0800

    Add Google auth

---
+++ components/Firebase.tsx
@@ -0,0 +1,26 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+
+const config = {
+  apiKey: 'AIzaSyARwwnx3bAANVZeOWhYp5XXpSw8ULe4gBs',
+  authDomain: 'theframedapp.firebaseapp.com',
+  projectId: 'theframedapp',
+  databaseURL: 'https://theframedapp.firebaseio.com',
+  storageBucket: 'theframedapp.appspot.com',
+  messagingSenderId: '1089862088680',
+  appId: '1:1089862088680:web:c984259c70dc19973c165d'
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+} else {
+  firebase.app()
+}
+
+const firestore = firebase.firestore()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export default firebase
+export{ firestore, provider }
